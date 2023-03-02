983150171067621ac5d09564340f6ee7bb6c6585
Mark Dostal <mhdostal@aol.com>
2023-03-02 15:25:42 -0600

    Initial Commit

---
+++ DevSummit2023_2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DevSummit2023_2
+//
+//  Created by Mark on 3/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DevSummit2023_2/DevSummit2023_2App.swift
@@ -0,0 +1,17 @@
+//
+//  DevSummit2023_2App.swift
+//  DevSummit2023_2
+//
+//  Created by Mark on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct DevSummit2023_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
